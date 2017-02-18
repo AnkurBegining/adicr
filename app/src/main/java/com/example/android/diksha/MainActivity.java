973bd2b0b77995207c7e9d00481aa151f2f83a9b
AnkurBeginning <201551016.iiitvadodara.ac.in@gmail.com>
2017-02-18 10:59:09 +0530

    fix: Read data from url using InputStreamReader and custom readFromStream Method

--- app/src/main/java/com/example/android/diksha/MainActivity.java
+++ app/src/main/java/com/example/android/diksha/MainActivity.java
@@ -5,11 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.charset.Charset;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -48,6 +51,8 @@ public class MainActivity extends AppCompatActivity {
             }
             HttpURLConnection urlConnection =(HttpURLConnection) url.openConnection();
 
+            //read data from url using InputStream class
+
             try {
                 InputStream in =new BufferedInputStream(urlConnection.getInputStream());
                 jsonResponse= readFromStream(in);
@@ -59,8 +64,20 @@ public class MainActivity extends AppCompatActivity {
 
         }
 
-        private String readFromStream(InputStream in) {
-            return null;
+        private String readFromStream(InputStream inputStream) throws IOException {
+            StringBuilder output=new StringBuilder();
+
+            if(inputStream !=null){
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String line = bufferedReader.readLine();
+                while (line != null){
+                    output.append(line);
+                    line =bufferedReader.readLine();
+                }
+
+            }
+            return output.toString();
         }
 
         private URL createUrl(String urlDomain) {
